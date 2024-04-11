4cbace275a4edeebfa43cc113e16aa30942c55fd
mairess <trabalho.maires@gmail.com>
2024-04-11 08:28:33 -0300

    fix: remove mocks from mocks files

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,9 +1,47 @@
 from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
-from tests.priority_queue.mocks import dict_one
-from tests.priority_queue.mocks import dict_two
-from tests.priority_queue.mocks import dict_three
+text_file_4_lines = [
+    "Acima de tudo,",
+    "é fundamental ressaltar que a adoção de",
+    "políticas descentralizadoras nos obriga",
+    "à análise do levantamento das variáveis envolvidas.",
+]
+
+dict_one = {
+    "nome_do_arquivo": "arquivo_4_lines.txt",
+    "qtd_linhas": 4,
+    "linhas_do_arquivo": text_file_4_lines,
+}
+
+
+text_file_6_lines = [
+    "Acima de tudo,",
+    "é fundamental ressaltar que a adoção de",
+    "políticas descentralizadoras nos obriga",
+    "à análise do levantamento das variáveis envolvidas.",
+    "à análise do levantamento das variáveis envolvidas.",
+    "à análise do levantamento das variáveis envolvidas.",
+]
+
+dict_two = {
+    "nome_do_arquivo": "arquivo_6_lines.txt",
+    "qtd_linhas": 6,
+    "linhas_do_arquivo": text_file_6_lines,
+}
+
+
+text_file_3_lines = [
+    "Acima de tudo,",
+    "é fundamental ressaltar que a adoção de",
+    "políticas descentralizadoras nos obriga",
+]
+
+dict_three = {
+    "nome_do_arquivo": "arquivo_3_lines.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": text_file_3_lines,
+}
 
 
 def test_basic_priority_queueing():
