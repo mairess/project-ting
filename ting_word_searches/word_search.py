7e1445562f45b41d03bc0232ee79bc1aa1e0efc8
mairess <trabalho.maires@gmail.com>
2024-04-11 07:57:35 -0300

    feat: implements exists_word and search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    data, result = instance.data, []
+
+    for dic in data:
+        ocorrencias, palavra, arquivo = [], word, dic["nome_do_arquivo"]
+
+        for index, line_value in enumerate(dic["linhas_do_arquivo"]):
+            if word.lower() in line_value.lower():
+                ocorrencias.append({"linha": index + 1})
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": palavra,
+                    "arquivo": arquivo,
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data, result = instance.data, []
+
+    for dic in data:
+        ocorrencias, palavra, arquivo = [], word, dic["nome_do_arquivo"]
+
+        for index, line_value in enumerate(dic["linhas_do_arquivo"]):
+            if word.lower() in line_value.lower():
+                ocorrencias.append(
+                    {"linha": index + 1, "conteudo": line_value}
+                )
+
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": palavra,
+                    "arquivo": arquivo,
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return result
